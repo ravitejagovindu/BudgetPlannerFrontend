38ea7dbb31135a183b1df0d375831f0f71ca7909
ravitejagovindu <68376518+ravitejagovindu@users.noreply.github.com>
2025-11-08 12:39:51 +0530

    mutualFunds fix

--- src/app/model/mutualFundHoldings.ts
+++ src/app/model/mutualFundHoldings.ts
@@ -5,7 +5,10 @@ export class MutualFundHolding {
     public folio: string,
     public quantity: number,
     public averagePrice: number,
+    public investedAmount: number,
     public lastPrice: number,
-    public pnl: number
+    public currentValue: number,
+    public pnl: number,
+    public pnlPercentage: number
   ) {}
 }
